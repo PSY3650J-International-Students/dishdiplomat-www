de81b814d90d5325ce32f77b0fbd4b5b5a92840d
Patrick Li <baichuanli@yahoo.com>
2023-06-21 00:38:48 +0800

    feat: new canteens added

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+      {
+        path: '/canteen/:canteen_path',
+        name: 'Canteen',
+        component: () => import('@/views/CanteenView.vue'),
+      }
     ],
   },
 ]
---
+++ src/views/CanteenView.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="text-center fill-height">
+      <h1 class="text-h2 font-weight-bold">{{canteen.name}}</h1>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import {useRoute} from 'vue-router'
+import { Canteen } from '@/types/Canteen'
+import { canteenList } from '@/store/CanteenList'
+import { ref } from 'vue'
+const route = useRoute()
+
+const canteen_path : string = route.params.canteen_path as string
+const canteen = ref<Canteen>(canteenList[0])
+for (var canteen_inst of canteenList) {
+  if (canteen_inst.path == canteen_path) {
+    canteen.value = canteen_inst
+  }
+}
+console.log(canteen_path)
+</script>

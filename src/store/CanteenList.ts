89f70563cab53a4549824ea5db47f1a3a47462e4
FS-LLY <136212969+FS-LLY@users.noreply.github.com>
2023-06-24 21:01:34 +0800

    Update CanteenList.ts

--- src/store/CanteenList.ts
+++ src/store/CanteenList.ts
@@ -1,6 +1,8 @@
 import { Canteen } from '@/types/Canteen'
 import { YulanGarden } from './canteens/YulanGarden'
+import { 4th Canteen } from './canteens/4th Canteen'
 
 export var canteenList: Canteen[] = [
   YulanGarden
+  4th Canteen
 ]

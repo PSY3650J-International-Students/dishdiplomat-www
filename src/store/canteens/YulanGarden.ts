ae95d155e2b4d04a43488a24c23d94768d58bcea
Patrick Li <baichuanli@yahoo.com>
2023-06-24 21:38:08 +0800

    fix: type error

--- src/store/canteens/YulanGarden.ts
+++ src/store/canteens/YulanGarden.ts
@@ -1,14 +1,8 @@
 import { Canteen } from '@/types/Canteen';
 import { CanteenWindow } from '@/types/CanteenWindow';
 
-const DumplingWindow: CanteenWindow = {
-  name: 'Dumplings',
-  path: 'dumplings',
-  cover_picture: 'https://i.imgur.com/2Z3QJ5v.jpg',
-}
-
 export const YulanGarden: Canteen = {
   name: 'Yulan Garden',
   path: 'yulan-garden',
-  window_list: [DumplingWindow, DumplingWindow, DumplingWindow]
+  window_list: []
 }

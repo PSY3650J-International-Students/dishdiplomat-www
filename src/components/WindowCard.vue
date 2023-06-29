184c22c9dc8afd35e502b132111207a5e27059a4
Li Leyao <li-leyao@sjtu.edu.cn>
2023-06-29 23:02:14 +0800

    no list

--- src/components/WindowCard.vue
+++ src/components/WindowCard.vue
@@ -15,41 +15,25 @@
   </v-card>
   <v-spacer />
   <v-dialog v-model="dialogOpen" persistent max-width="500px"
+                                            fullscreen
           activator="parent"
-          width="auto"
         >
           <v-card>
-            <v-card-text>
-              <v-card-title>
-              {{ props.window.name }}
-              </v-card-title>
-              <v-list>
-                <v-list-item v-for="dish in window.dishes" :key="dish.name">
-                    {{ dish.name }}
-                      <v-img
-                      :src="dish.cover_picture"
-                          width="100%"
-                          ></v-img>
-                    Chinese name:
-                    {{ dish.chinese_name }}
-                    <br>
-                    {{ dish.introduction }}
-                    <br>
-                    <template v-if="dish.flavor && dish.flavor.length > 0">
-                        Flavor:
-                        <br>
-                        <v-list>
-                          <v-list-item v-for="flavor in dish.flavor">
-                            {{ flavor }}
-                          </v-list-item>
-                        </v-list>
-                      </template>
-                  </v-list-item>
-              </v-list>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn color="primary" @click="closeDialog">Close</v-btn>
-            </v-card-actions>
+            <span>{{ '' }}</span>
+              <br>
+              <v-toolbar dense class="close-button-toolbar">
+                <v-btn icon @click="closeDialog">
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+                <v-spacer></v-spacer>
+              </v-toolbar>
+              <v-carousel height="500px">
+              <v-carousel-item v-for="dish in window.dishes" :key="dish.name">
+                <v-card-text>
+                  <DishPage :dish="dish" />
+                </v-card-text>              
+              </v-carousel-item>
+              </v-carousel>
           </v-card>
         </v-dialog>
 </template>

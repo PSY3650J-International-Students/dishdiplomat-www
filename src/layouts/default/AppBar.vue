235bf348e6c9a68ea11968bdf54345f61106b4d5
Patrick Li <baichuanli@yahoo.com>
2023-07-01 22:54:11 +0800

    feat: small text at the bottom of the nav bar

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -5,8 +5,9 @@
       <v-divider></v-divider>
       <v-list-item v-for="canteen in canteenListStore" :to=get_canteen_path(canteen) :title="canteen.name"></v-list-item>
       <v-divider></v-divider>
-      <v-list-item to="/issues">Report Any Issues or Contact Us</v-list-item>
+      <v-list-item to="/issues">Report Any Issues</v-list-item>
     </v-list>
+    <p class="text-center bottom">Made with ❤️ by SJTU local students</p>
   </v-navigation-drawer>
   <v-app-bar flat>
     <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
@@ -32,3 +33,14 @@
     return '/canteen/'+canteen.path
   }
 </script>
+
+<style scoped>
+  .bottom {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    color: grey;
+    font-size: .8em;
+  }
+</style>

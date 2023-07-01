14f24a61381445ba523e46e0f47c972fa0746c46
Patrick Li <baichuanli@yahoo.com>
2023-07-01 23:55:33 +0800

    feat: add a title to the issue page

--- src/views/IssueView.vue
+++ src/views/IssueView.vue
@@ -2,23 +2,24 @@
   <v-container class="fill-height">
     <v-list>
       <v-list-item>
-        <v-card>
-        <span>We apologize for any inconvenience caused by the errors you have encountered. Your satisfaction is our top priority, and we appreciate your patience as we work to resolve these issues. Please use this page to report any errors or problems you have experienced, and we will do our best to rectify them swiftly.</span>
-        </v-card>
+          <h1>Any issues?</h1>
       </v-list-item>
       <v-list-item>
-      <p>There are a few ways to report issues to us:</p>
+        <span>We apologize for any inconvenience caused by the errors you have encountered. Your satisfaction is our top priority, and we appreciate your patience as we work to resolve these issues. Please use this page to report any errors or problems you have experienced, and we will do our best to rectify them swiftly.</span>
+      </v-list-item>
+      <v-list-item>
+      <span>There are a few ways to report issues to us:</span>
       <ul>
         <li>Send us an email at <a href="mailto:public@patrickli.one">public@patrickli.one</a></li>
         <li>Send us a GitHub issues at <a href="https://github.com/PSY3650J-International-Students/dishdiplomat-www/issues/new">PSY3650J-International-Students/dishdiplomat-www</a></li>
       </ul>
       </v-list-item>
       <v-list-item>
-        <p>When reporting issues, please try to include as much information as possible.</p>
-        <p>If you are willing to become one of our contributors, please start by checking our code repository at <a href="https://github.com/PSY3650J-International-Students/dishdiplomat-www">PSY3650J-International-Students/dishdiplomat-www</a>. You can create a new pull request at <a href="https://github.com/PSY3650J-International-Students/dishdiplomat-www/pulls">https://github.com/PSY3650J-International-Students/dishdiplomat-www/pulls</a></p>
+        <span>When reporting issues, please try to include as much information as possible.</span>
+        <span>If you are willing to become one of our contributors, please start by checking our code repository at <a href="https://github.com/PSY3650J-International-Students/dishdiplomat-www">PSY3650J-International-Students/dishdiplomat-www</a>. You can create a new pull request at <a href="https://github.com/PSY3650J-International-Students/dishdiplomat-www/pulls">https://github.com/PSY3650J-International-Students/dishdiplomat-www/pulls</a></span>
       </v-list-item>
       <v-list-item>
-      <p>Thank you for your support!</p>
+      <span>Thank you for your support!</span>
       </v-list-item>
     </v-list>
   </v-container>

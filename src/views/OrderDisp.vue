e5552a1f816f80bb811e3b73dba9eb1d730f9514
Patrick Li <baichuanli@yahoo.com>
2023-07-01 12:20:08 +0800

    feat: display the order sentence

---
+++ src/views/OrderDisp.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container class="fill-height">
+    <p class="content fill-height">{{ msg }}</p>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  import { useRoute } from 'vue-router'
+  import { ref, watch } from 'vue'
+  const route = useRoute()
+  const msg = ref(route.params.order_text)
+
+  watch(() => route.params.order_text, (val) => {
+    msg.value = val
+  })
+
+
+
+
+  // canvas animation
+
+
+
+</script>
+
+<style scoped>
+.content {
+  font-size: 50vh;
+  display: flex;
+  white-space: nowrap;
+  overflow: scroll;
+}
+</style>
